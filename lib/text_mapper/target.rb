91a61784e0bd1c6be88c3f76105956b7fdcd4159
Mike Sassak <msassak@gmail.com>
2011-10-03 22:06:39 -0500

    Simplify receiver lookup
    Recurring to an arbitrary depth begs for misuse

--- lib/text_mapper/target.rb
+++ lib/text_mapper/target.rb
@@ -1,23 +1,17 @@
 module TextMapper
   class Target
-    attr_reader :target
+    attr_reader :name, :receiver
 
-    def initialize(target)
-      @target = target
+    def initialize(name, receiver=nil)
+      @name, @receiver = name, receiver
     end
 
     def call(ctx, args=[])
-      final_ctx, final_target = find_target(ctx, target)
-      final_ctx.send(final_target, *args)
-    end
-
-    private
-
-    def find_target(ctx, messages)
-      if messages.length == 1
-        [ctx, messages.first]
+      if receiver
+        final_ctx = ctx.send(receiver)
+        final_ctx.send(name, *args)
       else
-        find_target(ctx.send(messages.first), messages[1..-1])
+        ctx.send(name, *args)
       end
     end
   end
--- spec/text_mapper/target_spec.rb
+++ spec/text_mapper/target_spec.rb
@@ -5,30 +5,30 @@ module TextMapper
     it "sends a message to the context" do
       ctx = double("execution context")
       ctx.should_receive(:foo)
-      to = Target.new([:foo])
+      to = Target.new(:foo)
       to.call(ctx)
     end
 
     it "sends a message with one argument to the context" do
       ctx = double("execution context")
       ctx.should_receive(:foo).with(:bar)
-      to = Target.new([:foo])
+      to = Target.new(:foo)
       to.call(ctx, [:bar])
     end
 
     it "sends a message with two arguments to the context" do
       ctx = double("execution context")
       ctx.should_receive(:foo).with(:bar, :baz)
-      to = Target.new([:foo])
+      to = Target.new(:foo)
       to.call(ctx, [:bar, :baz])
     end
 
-    it "sends a message to an attribute of the context" do
+    it "sends a message to a receiver in the context" do
       target = double("target attribute")
       target.should_receive(:foo)
       ctx = double("execution context")
       ctx.stub(:target).and_return(target)
-      to = Target.new([:target, :foo])
+      to = Target.new(:foo, :target)
       to.call(ctx)
     end
   end
